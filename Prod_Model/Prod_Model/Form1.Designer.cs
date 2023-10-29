0acaeb853ebcc8b9967263e7807720f55d0cd32d
PrAl4 <igorsobaka4@gmail.com>
2023-10-30 00:31:17 +0300

    Prod_Model come rules, some elements and functions

--- Prod_Model/Prod_Model/Form1.Designer.cs
+++ Prod_Model/Prod_Model/Form1.Designer.cs
@@ -28,20 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Direct_output = new System.Windows.Forms.Button();
+            this.Reverse_output = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Check_elem = new System.Windows.Forms.CheckedListBox();
+            this.All_rules = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.allRules = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.text_rulles = new System.Windows.Forms.TextBox();
+            this.Close_rulles = new System.Windows.Forms.Button();
+            this.resultes = new System.Windows.Forms.CheckedListBox();
+            this.all_results = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // Direct_output
+            // 
+            this.Direct_output.Location = new System.Drawing.Point(13, 14);
+            this.Direct_output.Name = "Direct_output";
+            this.Direct_output.Size = new System.Drawing.Size(174, 73);
+            this.Direct_output.TabIndex = 0;
+            this.Direct_output.Text = "Прямой вывод";
+            this.Direct_output.UseVisualStyleBackColor = true;
+            this.Direct_output.Click += new System.EventHandler(this.Direct_output_Click);
+            // 
+            // Reverse_output
+            // 
+            this.Reverse_output.Location = new System.Drawing.Point(13, 93);
+            this.Reverse_output.Name = "Reverse_output";
+            this.Reverse_output.Size = new System.Drawing.Size(174, 73);
+            this.Reverse_output.TabIndex = 1;
+            this.Reverse_output.Text = "Обратный вывод";
+            this.Reverse_output.UseVisualStyleBackColor = true;
+            this.Reverse_output.Click += new System.EventHandler(this.Reverse_output_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(13, 567);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(174, 73);
+            this.Clear.TabIndex = 2;
+            this.Clear.Text = "Очистить";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Check_elem
+            // 
+            this.Check_elem.FormattingEnabled = true;
+            this.Check_elem.Location = new System.Drawing.Point(520, 48);
+            this.Check_elem.Name = "Check_elem";
+            this.Check_elem.Size = new System.Drawing.Size(294, 599);
+            this.Check_elem.TabIndex = 3;
+            // 
+            // All_rules
+            // 
+            this.All_rules.Location = new System.Drawing.Point(12, 205);
+            this.All_rules.Name = "All_rules";
+            this.All_rules.Size = new System.Drawing.Size(174, 73);
+            this.All_rules.TabIndex = 4;
+            this.All_rules.Text = "Показать все правила";
+            this.All_rules.UseVisualStyleBackColor = true;
+            this.All_rules.Click += new System.EventHandler(this.All_rules_Click);
+            // 
+            // allRules
+            // 
+            this.allRules.Location = new System.Drawing.Point(208, 48);
+            this.allRules.Multiline = true;
+            this.allRules.Name = "allRules";
+            this.allRules.Size = new System.Drawing.Size(294, 599);
+            this.allRules.TabIndex = 5;
+            this.allRules.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(520, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(294, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Все компоненты";
+            // 
+            // text_rulles
+            // 
+            this.text_rulles.Location = new System.Drawing.Point(208, 14);
+            this.text_rulles.Name = "text_rulles";
+            this.text_rulles.Size = new System.Drawing.Size(294, 22);
+            this.text_rulles.TabIndex = 7;
+            this.text_rulles.Text = "Все правила";
+            this.text_rulles.Visible = false;
+            // 
+            // Close_rulles
+            // 
+            this.Close_rulles.Location = new System.Drawing.Point(13, 284);
+            this.Close_rulles.Name = "Close_rulles";
+            this.Close_rulles.Size = new System.Drawing.Size(174, 73);
+            this.Close_rulles.TabIndex = 8;
+            this.Close_rulles.Text = "Скрыть все правила";
+            this.Close_rulles.UseVisualStyleBackColor = true;
+            this.Close_rulles.Click += new System.EventHandler(this.Close_rulles_Click);
+            // 
+            // resultes
+            // 
+            this.resultes.FormattingEnabled = true;
+            this.resultes.Location = new System.Drawing.Point(835, 48);
+            this.resultes.Name = "resultes";
+            this.resultes.Size = new System.Drawing.Size(294, 599);
+            this.resultes.TabIndex = 9;
+            this.resultes.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.resultes_ItemCheck);
+            // 
+            // all_results
+            // 
+            this.all_results.Location = new System.Drawing.Point(835, 14);
+            this.all_results.Name = "all_results";
+            this.all_results.Size = new System.Drawing.Size(294, 22);
+            this.all_results.TabIndex = 10;
+            this.all_results.Text = "Все исходы";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1105, 593);
+            this.ClientSize = new System.Drawing.Size(1580, 652);
+            this.Controls.Add(this.all_results);
+            this.Controls.Add(this.resultes);
+            this.Controls.Add(this.Close_rulles);
+            this.Controls.Add(this.text_rulles);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.allRules);
+            this.Controls.Add(this.All_rules);
+            this.Controls.Add(this.Check_elem);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Reverse_output);
+            this.Controls.Add(this.Direct_output);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "League of Legends Items";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Direct_output;
+        private System.Windows.Forms.Button Reverse_output;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.CheckedListBox Check_elem;
+        private System.Windows.Forms.Button All_rules;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox allRules;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox text_rulles;
+        private System.Windows.Forms.Button Close_rulles;
+        private System.Windows.Forms.CheckedListBox resultes;
+        private System.Windows.Forms.TextBox all_results;
     }
 }
 
--- Prod_Model/Prod_Model/Form1.cs
+++ Prod_Model/Prod_Model/Form1.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +18,175 @@ namespace Prod_Model
         public Form1()
         {
             InitializeComponent();
+            FillTextBox();
+            FillCheckBox();
         }
+
+        //Предметы крафта
+        string[] Elements = new string[120];
+
+        //Готовые продукты
+        string[] Products = new string[190];
+
+        //Рецепты
+        string[] Recepies = new string[200];
+
+
+        //Функция заполнения массивов
+        void FillElements()
+        {
+            int cp = 0;
+            int ce = 0;
+            StreamReader file = File.OpenText(@"D:\ISlabs\Prod_Model\Prod_Model\Prod_Model\Properties\elems.txt");
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] items = line.Split('.');
+                for (int i = 0; i < items.Length; i++)
+                {
+                    if (items[i] == "" || items[i] == " ")
+                    {
+                        continue;
+                    }
+                    if (items[i].ToCharArray()[0] == '*')
+                    {
+                        Products[cp] = items[i].Substring(1);
+                        cp++;
+                    }
+                    else if (items[i].ToCharArray()[0] == '^')
+                    {
+                        Products[cp] = items[i].Substring(1);
+                        cp++;
+                        Elements[ce] = items[i].Substring(1);
+                        ce++;
+                    }
+                    else
+                    {
+                        Elements[ce] = items[i];
+                        ce++;
+                    }
+                }
+            }
+            MessageBox.Show(cp.ToString());
+            file.Close();
+        }
+
+        //Заполнение массива правилами
+        void FillRulles()
+        {
+            int cr = 0;
+            StreamReader file = File.OpenText(@"D:\ISlabs\Prod_Model\Prod_Model\Prod_Model\Properties\rules.txt");
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] items = line.Split('.');
+                for (int i = 0; i < items.Length; i++)
+                {
+                    if (items[i] == "" || items[i] == " ")
+                    {
+                        continue;
+                    }
+                    Recepies[cr] = items[i];
+                    cr++;
+                }
+            }
+            file.Close();
+        }
+
+        //Функция заполнения 
+        void FillCheckBox()
+        {
+            FillElements();
+            for(int i = 0; i < Elements.Length; i++)
+            {
+                if (Elements[i] == null || Elements[i] == "")
+                {
+                    continue;
+                }
+                Check_elem.Items.Add(Elements[i]);
+            }
+            for (int i = 0; i < Products.Length; i++)
+            {
+                if (Products[i] == null || Products[i] == "")
+                {
+                    continue;
+                }
+                resultes.Items.Add(Products[i]);
+            }
+        }
+
+        //Заполнения текстбокса правилами
+        void FillTextBox()
+        {
+            FillRulles();
+            allRules.ScrollBars = ScrollBars.Vertical;
+            for (int i = 0; i < Recepies.Length; i++)
+            {
+                if (Recepies[i] == null || Recepies[i] == "")
+                {
+                    continue;
+                }
+                allRules.AppendText(Recepies[i]);
+                allRules.Text += Environment.NewLine + Environment.NewLine;
+            }
+        }
+
+        //Кнопка очищение всего
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Check_elem.ClearSelected();
+            for(int i = 0; i < Check_elem.Items.Count; i++)
+            {
+                Check_elem.SetSelected(i, false);
+                Check_elem.SetItemChecked(i, false);
+            }
+            resultes.ClearSelected(); 
+            for (int i = 0; i < resultes.Items.Count; i++)
+            {
+                resultes.SetSelected(i, false);
+                resultes.SetItemChecked(i, false);
+            }
+        }
+
+        //Показать все правила
+        private void All_rules_Click(object sender, EventArgs e)
+        {
+            allRules.Visible = true;
+            text_rulles.Visible = true;
+        }
+
+        //Обратный вывод
+        private void Reverse_output_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Прямой вывод
+        private void Direct_output_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Скрыть все правила
+        private void Close_rulles_Click(object sender, EventArgs e)
+        {
+            allRules.Visible = false;
+            text_rulles.Visible = false;
+        }
+
+        private void resultes_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if(resultes.CheckedItems.Count > 0)
+            {
+                for (int i = 0; i < resultes.Items.Count; ++i)
+                {
+                    if(e.Index != i)
+                    {
+                        resultes.SetItemChecked(i, false);
+                    }
+                }
+            }
+        }
+
     }
 }
